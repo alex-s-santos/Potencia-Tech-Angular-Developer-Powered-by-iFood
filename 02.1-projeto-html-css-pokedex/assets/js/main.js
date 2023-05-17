7804cd60c28c29293222ba982889a2843d0ed288
Alex Santos <81884737+alex-s-santos@users.noreply.github.com>
2023-05-16 23:35:13 -0300

    Add files via upload
    Subindo novos arquivos atualizados

--- 02.1-projeto-html-css-pokedex/assets/js/main.js
+++ 02.1-projeto-html-css-pokedex/assets/js/main.js
@@ -1,14 +1,33 @@
+const pokemons = document.querySelector('#pokemons');
 
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+function converterJsonEmLi(pokemon){
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">
+                        grass
+                    </li>
+                    <li class="type">
+                        poison
+                    </li>
+                </ol>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="Foto pokédex ${pokemon.name}">
+            </div>
+        </li>
+    `
+}
 
-
-fetch(url).then(function(response) {
-    response.json().then(function(responseBody){
-        console.log(responseBody);
-    })
-
-
-});
+pokeApi.getPokemonsList().then((pokemonsList = []) => {
+    const newPokemonList = pokemonsList.map((value) => converterJsonEmLi(value));
+    const newHTML = newPokemonList.join(""); //Retira a vírgula padrão
+    pokemons.innerHTML = newHTML;
+    
+}).catch((error) => {
+    pokemons.innerHTML = 'Desculpe, a pokédex não está funcionando no momento 😞<br>';
+    pokemons.innerHTML += `Causa do erro: ${error}`;
+    pokemons.innerHTML += '<img src="./assets/images/error-pokedex.jpg" style="border-radius: 1rem">';
+})
 
